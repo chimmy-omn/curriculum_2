007ddbdec004bef26ab8eedc5f3bed922b88cf36
chimmy-omn <81784526+chimmy-omn@users.noreply.github.com>
2021-06-17 13:32:20 +0900

    課題の提出
    2-8の提出

---
+++ 2-8/2-8.php
@@ -0,0 +1,11 @@
+
+<?php
+$fruits = ["apple" => "りんご", "orenge" => "みかん", "peach" => "もも"];
+
+foreach ($fruits as $key => $value) {
+    echo $key;
+    echo "といったら";
+    echo $value;
+    echo '<br>';
+
+}
